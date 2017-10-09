e2c03e899bc24001fa722760f9c10cecf8cc451c
Andy-chavez <32040585+Andy-chavez@users.noreply.github.com>
2017-10-09 16:23:38 -0300

    Add files via upload

---
+++ Untitled1.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#define inf 2147483647
+using namespace std;
+//Algoritmo de dijkstra
+//https://github.com/jariasf/Online-Judges-Solutions/blob/master/Algorithms/C%2B%2B/Graph%20Theory/Algoritmo%20de%20Dijkstra.cpp
+//Dinamico ej. int *t=new int(); dps delete t;
+//Estructuras enlazadas: tiene su info + su siguiente, 
+//que no necesariamente esta ordenada por su orden de siguiente
+//https://www.youtube.com/watch?v=rGcrN82_pCU&feature=youtu.be
+
+bool c1;
+bool c2;
+bool c3;
+bool c4;
+bool c5;
+bool c6;
+bool c7;
+
+struct nodo
+{
+	float dist;
+	int distancias[8];
+	nodo *nodos[8];
+	
+};
+
+ 
+int main()
+{
+		
+	return 0;
+}
